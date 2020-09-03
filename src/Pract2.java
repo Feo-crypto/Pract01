3eeb9c7f30e5ced85b2595068e52f0c44c4ddcd3
Feo-crypto <beerhunter922@gmail.com>
2020-09-03 11:53:10 +0300

    creation

---
+++ src/Pract2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pract2 {
+    public static long Factorial(long n){
+        if (n<=0){
+            return 1;
+
+        }else {
+            return n*Factorial(n-1);
+        }
+    }
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        long number=input.nextLong();
+        System.out.println(Factorial(number));
+    }
+}
